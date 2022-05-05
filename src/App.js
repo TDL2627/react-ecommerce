0011668e51699b830211a47e46a9de414d846720
TDL2627 <the.dark.lord.2627@gmail.com>
2022-05-05 13:09:35 +0200

    making it more readable

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import Home from "./routes/home/home.component";
-const App =()=> {
-return <Home />;
+import {Routes, Route, Outlet} from 'react-router-dom'
 
+import Home from "./routes/home/home.component";
+
+const Navigation = () => {
+  return(
+    <div>
+      <div>
+        <h1>I am the navigation</h1>
+      </div>
+      <Outlet/>
+    </div>
+  );
+};
+
+const Shop = () => {
+return <h1>The shop</h1>
+};
+
+
+const App =()=> {
+
+return (
+  <Routes>
+    <Route path='/' element={<Navigation/>}>
+    <Route index element={<Home/>}/>
+    <Route path='shop' element={<Shop/>}/>
+  </Route>
+  </Routes>
+)
 }
 
 export default App;
