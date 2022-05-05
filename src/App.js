57896e4e58f9249113a5d1e32a955146ac750e3d
TDL2627 <the.dark.lord.2627@gmail.com>
2022-05-05 13:25:55 +0200

    making it more readable

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
-import {Routes, Route, Outlet} from 'react-router-dom'
+import {Routes, Route} from 'react-router-dom'
 
 import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component"
 
-const Navigation = () => {
-  return(
-    <div>
-      <div>
-        <h1>I am the navigation</h1>
-      </div>
-      <Outlet/>
-    </div>
-  );
-};
 
 const Shop = () => {
 return <h1>The shop</h1>
---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,25 @@
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
+
+const Navigation = () => {
+    return(
+      <Fragment>
+
+    
+        <div className="navigation">
+            <div>Logo</div>
+            <div className="links-container">
+                <Link className="nav-link" to='/shop'>
+                Shop
+                </Link>
+            </div>
+          <h1>I am the navigation</h1>
+        </div>
+        <Outlet/>
+
+
+        </Fragment>
+    );
+  };
+
+export default Navigation;
