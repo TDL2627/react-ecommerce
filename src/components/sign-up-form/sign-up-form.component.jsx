28208ce0404b89c445dce00434d8cd8f7bfe36f5
TDL2627 <the.dark.lord.2627@gmail.com>
2022-05-06 10:54:25 +0200

    added a form

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,19 +1,41 @@
+import  {useState} from 'react';
+
+const defaultFormFields = {
+    displayName:'',
+    email:'',
+    password:'',
+    confirmPassword:''
+}
+
+
 const SignUpForm = () => {
+    const [formFields, setFormFields]=useState(defaultFormFields);
+    const { displayName, email, password, confirmPassword}= formFields;
+
+    console.log(formFields)
+
+const handleChange = (event )=>{
+const {name, value}=event.target;
+
+setFormFields({...formFields, [name]: value })
+
+};
+
     return(
         <div>
      <h1>Sign Up</h1>
      <form onSubmit={()=>{}}>
          <label>Display Name</label>
-         <input type="text" required/>
+         <input type="text" required onChange={handleChange} value={displayName} name='displayName'/>
 
          <label>Email</label>
-         <input type="email" required/>
+         <input type="email" required onChange={handleChange} value={email} name='email'/>
 
          <label>Password</label>
-         <input type="password" required/>
+         <input type="password" required onChange={handleChange} value={password} name='password'/>
 
          <label>Confirm Password</label>
-         <input type="pasword" required/>
+         <input type="pasword" required onChange={handleChange} value={confirmPassword}  name='confirmPassword'/>
 
          <button type="submit">Submit</button>
      </form>
